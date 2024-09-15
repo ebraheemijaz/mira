16765e623f09b22ed9f5f373d17558b3cdd3bd7e
ebraheem <ebraheemijaz913@gmail.com>
2024-09-15 15:48:57 +0500

    bug fix

--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { DetailsContect } from "./InfoContext";
 
 export default function Choose() {
   const details = useContext(DetailsContect);
@@ -6,23 +7,29 @@ export default function Choose() {
   return (
     <section id="services" class="cards section">
       <div class="container section-title" data-aos="fade-up">
-        <h2>{details?.lang === 'EN' ? "Services" :"خدمات"}</h2>
-        <p>{details?.lang === 'EN' ? `we provide best professional services` :`نحن نقدم أفضل الخدمات المهنية`}</p>
+        <h2>{details?.lang === "EN" ? "Services" : "خدمات"}</h2>
+        <p>
+          {details?.lang === "EN"
+            ? `we provide best professional services`
+            : `نحن نقدم أفضل الخدمات المهنية`}
+        </p>
       </div>
 
       <div class="container">
         <div class="row gy-4">
           <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
             <div class="card-item">
-              <span>{details?.lang === 'EN' ? "01" :"واحد"}</span>
+              <span>{details?.lang === "EN" ? "01" : "واحد"}</span>
               <h4>
                 <a href="" class="stretched-link">
-                {details?.lang === 'EN' ? "Fast charging" :"شحن سريع"}
+                  {details?.lang === "EN" ? "Fast charging" : "شحن سريع"}
                 </a>
               </h4>
               <p>
-              {details?.lang === 'EN' ? `Express shipping for orders +10,000 Saudi riyals for all
-                categories except cables and wires within Jeddah and Riyadh` : `شحن سريع للطلبات +10,000 ريال سعودي للجميع
+                {details?.lang === "EN"
+                  ? `Express shipping for orders +10,000 Saudi riyals for all
+                categories except cables and wires within Jeddah and Riyadh`
+                  : `شحن سريع للطلبات +10,000 ريال سعودي للجميع
                 الفئات باستثناء الكابلات والأسلاك داخل جدة والرياض`}
               </p>
             </div>
@@ -30,15 +37,17 @@ export default function Choose() {
 
           <div class="col-lg-4" data-aos="fade-up" data-aos-delay="200">
             <div class="card-item">
-              <span>{details?.lang === 'EN' ? "02" :"إثنان"}</span>
+              <span>{details?.lang === "EN" ? "02" : "إثنان"}</span>
               <h4>
                 <a href="" class="stretched-link">
                   Secure payment methods
                 </a>
               </h4>
               <p>
-              {details?.lang === 'EN' ? `We are committed to providing the highest level of quality and
-                satisfaction to our customers.` : `نحن ملتزمون بتقديم أعلى مستوى من الجودة و
+                {details?.lang === "EN"
+                  ? `We are committed to providing the highest level of quality and
+                satisfaction to our customers.`
+                  : `نحن ملتزمون بتقديم أعلى مستوى من الجودة و
                 رضا عملائنا.`}
               </p>
             </div>
@@ -46,13 +55,19 @@ export default function Choose() {
 
           <div class="col-lg-4" data-aos="fade-up" data-aos-delay="300">
             <div class="card-item">
-              <span>{details?.lang === 'EN' ? "03" :"ثلاثة"}</span>
+              <span>{details?.lang === "EN" ? "03" : "ثلاثة"}</span>
               <h4>
                 <a href="" class="stretched-link">
-                {details?.lang === 'EN' ? `Technical support around the clock` : ` الدعم الفني على مدار الساعة`}
+                  {details?.lang === "EN"
+                    ? `Technical support around the clock`
+                    : ` الدعم الفني على مدار الساعة`}
                 </a>
               </h4>
-              <p>{details?.lang === 'EN' ? `Enjoy an exciting and unique shopping experience` : `استمتع بتجربة تسوق مثيرة وفريدة من نوعها`}</p>
+              <p>
+                {details?.lang === "EN"
+                  ? `Enjoy an exciting and unique shopping experience`
+                  : `استمتع بتجربة تسوق مثيرة وفريدة من نوعها`}
+              </p>
             </div>
           </div>
         </div>
